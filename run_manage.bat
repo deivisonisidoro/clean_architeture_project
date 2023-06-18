e5313674ec6dc847d551af122ed1a6c9f4690a26
Deivison Isidoro da Silva <61429017+deivisonisidoro@users.noreply.github.com>
2023-06-18 12:56:03 -0300

    deploy: fixing the .bat file

--- run_manage.bat
+++ run_manage.bat
@@ -1,2 +1,10 @@
 @echo off
-python infrastructure\django_core\manage.py %*
+
+:: Get the absolute path of the script's directory
+for %%I in ("%~dp0") do set "SCRIPT_DIR=%%~fI"
+
+:: Set the Python path to the project root directory
+set PYTHONPATH=%SCRIPT_DIR%
+
+:: Run the manage.py script
+python "%SCRIPT_DIR%\infrastructure\django_core\manage.py" %*
